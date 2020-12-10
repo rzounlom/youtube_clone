6275af759e77ce4ab9c4f721a84a29582b71867e
rzounlome <rzounlome@harmonyhit.com>
2020-12-10 11:45:13 -0500

    updated ui styles

--- src/components/RecommendedVideos/RecommendedVideos.jsx
+++ src/components/RecommendedVideos/RecommendedVideos.jsx
@@ -71,6 +71,70 @@ const RecommendedVideos = () => {
           channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
           timestamp="3 days ago"
         />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
+        <VideoCard
+          title="lofi hip hop radio - beates to study/relax to"
+          channel="Chillhop Music"
+          views="3.4k Views"
+          image="https://i.pinimg.com/originals/98/a9/32/98a932fe637d900110be28ffdf221398.png"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjahr0xia_SVFNHaYCIwqW1cv4ZaAnyQadElwB_nA=s48-c-k-c0xffffffff-no-rj-mo"
+          timestamp="3 days ago"
+        />
       </div>
     </div>
   );
