ab91efe87c196153188d51f47e5c40f20709aca2
rzounlome <rzounlome@harmonyhit.com>
2020-12-11 08:20:49 -0500

    styles cleanup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import { useState, useEffect } from "react";
 import { youtube } from "./api/axios";
 
@@ -24,13 +25,14 @@ const App = () => {
     } catch (error) {}
   }, [videos]);
   return (
-    <div className="app">
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
-    </div>
+    // <div className="app">
+    //   <Header />
+    //   <div className="app__page">
+    //     <Sidebar />
+    //     <RecommendedVideos />
+    //   </div>
+    // </div>
+    <HomeScreen />
   );
 };
 
